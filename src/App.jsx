4412cea7255c9059dfbe7f05fb7384ddf86dd7d6
MR-ZIA-1 <mrziaurrehman01@gmail.com>
2024-09-12 11:51:45 -0700

    Fix case sensitivity issue for Roadmap component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Benefits from './components/Benefits';
 import Collaboration from './components/Collaboration';
 import Services from './components/Services';
 import Pricing from './components/Pricing';
-import RoadMap from './components/RoadMap';
+import Roadmap from './components/Roadmap';
 import Footer from './components/Footer';
 
 
